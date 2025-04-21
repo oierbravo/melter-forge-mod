790736d67bc3ed65bff7bde78bcfaa59f9c476f5
Oier Bravo Urtasun <oierbravo@gmail.com>
2025-04-21 17:14:21 +0200

    - Leaves to water recipe

--- src/main/java/com/oierbravo/melter/foundation/data/MeltingRecipeGen.java
+++ src/main/java/com/oierbravo/melter/foundation/data/MeltingRecipeGen.java
@@ -6,6 +6,9 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.material.Fluids;
 import net.neoforged.neoforge.common.Tags;
 
@@ -39,6 +42,50 @@ public class MeltingRecipeGen extends RecipeProvider {
                 .withProcessingTime(400)
                 .requiredHeat(5)
                 .save(recipeOutput);
+
+        create("honey")
+                .input(Blocks.HONEY_BLOCK)
+                .output(AllFluids.HONEY.get(), 1000)
+                .withProcessingTime(400)
+                .requiredHeat(1)
+                .whenModLoaded("create")
+                .save(recipeOutput);
+
+        create("snow_ball")
+                .input(Items.SNOWBALL)
+                .output(Fluids.WATER.getSource(), 250)
+                .withProcessingTime(400)
+                .requiredHeat(1)
+                .save(recipeOutput);
+
+        create("snow")
+                .input(Blocks.SNOW_BLOCK)
+                .output(Fluids.WATER.getSource(), 500)
+                .withProcessingTime(400)
+                .requiredHeat(1)
+                .save(recipeOutput);
+
+        create("ice")
+                .input(Blocks.ICE)
+                .output(Fluids.WATER.getSource(), 1000)
+                .withProcessingTime(400)
+                .requiredHeat(1)
+                .save(recipeOutput);
+
+        create("blue_ice")
+                .input(Blocks.BLUE_ICE)
+                .output(Fluids.WATER.getSource(), 1000)
+                .withProcessingTime(400)
+                .requiredHeat(1)
+                .save(recipeOutput);
+
+        create("leaves")
+                .input(ItemTags.LEAVES)
+                .output(Fluids.WATER.getSource(), 100)
+                .withProcessingTime(400)
+                .requiredHeat(1)
+                .save(recipeOutput);
+
 
     }
 
