8bb3baeb7567f5f56abc677acee4fc3f77fe426e
Ian <ianmcg1128@gmail.com>
2026-06-21 05:37:43 -0500

    updates to latest KubeJS (#22)

--- src/main/java/com/oierbravo/melter/compat/kubejs/HeatSourceEvent.java
+++ src/main/java/com/oierbravo/melter/compat/kubejs/HeatSourceEvent.java
@@ -1,11 +1,19 @@
 package com.oierbravo.melter.compat.kubejs;
 
+import com.oierbravo.melter.content.melter.heatsource.HeatSource;
+import com.oierbravo.melter.content.melter.heatsource.HeatSourcesRegistry;
+import com.oierbravo.melter.foundation.utility.BlockPredicateUtils;
 import dev.latvian.mods.kubejs.event.KubeEvent;
+import dev.latvian.mods.kubejs.recipe.component.UniqueIdBuilder;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 
 public class HeatSourceEvent implements KubeEvent {
     public void add(ResourceLocation res, int pHeatLevel, String pSourceType ) {
-        /*Block block = BuiltInRegistries.BLOCK.get(res);
+        Block block = BuiltInRegistries.BLOCK.get(res);
         UniqueIdBuilder uniqueBuilder = new UniqueIdBuilder(new StringBuilder());
         uniqueBuilder.append(res);
         String uniqueId = uniqueBuilder.build();
@@ -13,7 +21,7 @@ public class HeatSourceEvent implements KubeEvent {
         new RegistrySetBuilder()
                 .add(HeatSourcesRegistry.HEAT_SOURCE_REGISTRY_KEY, bootstrap -> {
                     // Register configured features through the bootstrap context (see below)
-                    bootstrap.register(resourceKey, new HeatSource(block, pHeatLevel, HeatSource.SourceType.valueOf(pSourceType)));
-                });*/
+                    bootstrap.register(resourceKey, new HeatSource(BlockPredicateUtils.Builder.build(block), pHeatLevel, HeatSource.SourceType.valueOf(pSourceType)));
+                });
     }
 }
